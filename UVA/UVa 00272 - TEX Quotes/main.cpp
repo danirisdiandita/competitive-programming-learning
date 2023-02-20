43d64470ba896fcd6ac05e3fb357ebef973c089f
Norma Dani Risdiandita <dani@192.168.1.7>
2023-02-21 06:40:41 +0700

    add tex quotes solution

--- UVA/UVa 00272 - TEX Quotes/main.cpp
+++ UVA/UVa 00272 - TEX Quotes/main.cpp
@@ -4,15 +4,34 @@
 
 using namespace std; 
 int main() {
-
-    int i; // iterator 
+    bool open = true; 
     string strinput; 
 
-    freopen("input.txt", "r", stdin);
-
-
+    // freopen("input.txt", "r", stdin);
     while(getline(cin, strinput)) {
-        strinput.find()
+
+        int len = strinput.length(); 
+
+        for (int i = 0; i < len ; i++) {
+            if (strinput[i] == '"') {
+                if (open) {
+                    cout << "``"; 
+                    open = !open; 
+                } else {
+                    cout << "''"; 
+                    open = !open; 
+                }
+
+
+            } else {
+                cout << strinput[i]; 
+            }
+
+            
+        }
+
+        cout << "" << endl; 
+        // cout << strinput << endl; 
     }
 
     return 0; 
