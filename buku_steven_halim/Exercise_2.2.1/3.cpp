c734d25c0bd05e68e180097a4dd4181113c49edc
dani <admin@widyawicara.com>
2023-11-23 06:51:02 +0700

    .

---
+++ buku_steven_halim/Exercise_2.2.1/3.cpp
@@ -0,0 +1,14 @@
+// using two pointers for sorted array 
+
+
+
+#include <vector> 
+#include <bits/stdc++.h>
+
+int main() {
+    int l, r; 
+    std::vector<int> arr = { 1, 4, 45, 6, 10, -8 };
+    
+
+
+}
