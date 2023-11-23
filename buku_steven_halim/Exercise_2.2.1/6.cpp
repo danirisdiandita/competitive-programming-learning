7fe9685912fde95c14f342ffdf8ffdd0d2a93d45
Norma Dani Risdiandita <dani@Normas-MacBook-Air.local>
2023-11-23 12:21:51 +0700

    add median

---
+++ buku_steven_halim/Exercise_2.2.1/6.cpp
@@ -0,0 +1,11 @@
+
+#include <vector>
+#include <iostream>
+
+
+int main()
+{
+    std::vector<int> arr = {1, 5, 8, 9, 6, 7, 3, 4, 2, 0, 11};
+    std::cout << arr[arr.size() / 2] << std::endl;
+   
+}
